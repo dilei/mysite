21e273a444467a51acbea70d8f89ca765a126f8b
root <root@iZwz90qokcxygcqwhd7j6kZ.(none)>
2017-03-08 16:01:09 +0800

    update

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -1,16 +1,11 @@
 <?php
 /**
- * @name IndexController
- * @author root
- * @desc 默认控制器
- * @see http://www.php.net/manual/en/class.yaf-controller-abstract.php
+ * 默认控制器
  */
-class IndexController extends Yaf_Controller_Abstract {
+class IndexController extends BaseController {
 
 	/** 
      * 默认动作
-     * Yaf支持直接把Yaf_Request_Abstract::getParam()得到的同名参数作为Action的形参
-     * 对于如下的例子, 当访问http://yourhost/mysite/index/index/index/name/root 的时候, 你就会发现不同
      */
 	public function indexAction($name = "Stranger") {
 		//1. fetch query
@@ -26,4 +21,5 @@ class IndexController extends Yaf_Controller_Abstract {
 		//4. render by Yaf, 如果这里返回FALSE, Yaf将不会调用自动视图引擎Render模板
         return TRUE;
 	}
+
 }
