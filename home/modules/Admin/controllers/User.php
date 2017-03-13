bd1e3d507d77add653fffd8cf6b767f385ff6b04
dilei <1365799067@qq.com>
2017-03-13 22:15:29 +0800

    udpate

--- home/modules/Admin/controllers/User.php
+++ home/modules/Admin/controllers/User.php
@@ -12,7 +12,7 @@ class UserController extends BaseController {
 		$get = $this->getRequest()->getQuery("get", "default value");
 
 		//2. fetch model
-		$model = new SampleModel();
+		$model = new Admin_SampleModel();
 
 		//3. assign
 		$this->getView()->assign("content", $model->selectSample());
