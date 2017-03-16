716c03b6ca7691bc76f6442aa184ee4c48f802f7
dilei <1365799067@qq.com>
2017-03-16 11:57:27 +0800

    update

--- home/controllers/Index.php
+++ home/controllers/Index.php
@@ -18,8 +18,29 @@ class IndexController extends BaseController {
 		$this->getView()->assign("content", $model->selectSample());
 		$this->getView()->assign("name", $name);
 
+        // $this->setCrontab();
+        $this->delCrontab();
+
 		//4. render by Yaf, 如果这里返回FALSE, Yaf将不会调用自动视图引擎Render模板
         return TRUE;
 	}
+
+    public function setCrontab() {
+        $crontab = new CrontabManager();
+        $job = $crontab->newJob();
+        $job->on('* * * * *');
+        $job->onMinute('20-30')->doJob("echo foo");
+        $crontab->add($job);
+        $job->onMinute('35-40')->doJob("echo bar");
+        $crontab->add($job);
+        $crontab->save();
+    }
+
+    public function delCrontab() {
+        $crontab = new CrontabManager();
+        $crontab->enableOrUpdate('/tmp/cronfile.txt');
+        // $crontab->disable('/tmp/cronfile.txt');
+        $crontab->save();
+    }
 
 }
