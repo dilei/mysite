c64b5689993d2431f0e9ade14a2114d4515910f5
lei.di <lei.di@huanxi.com>
2017-03-21 18:22:26 +0800

    update

--- cli_index.php
+++ cli_index.php
@@ -6,6 +6,12 @@ $request = new Yaf_Request_Simple("CLI", $module, $controller, $action, array("n
 // print_r($request);
 
 define('APPLICATION_PATH', dirname(__FILE__));
-$app = new Yaf_Application("conf/home.ini");
+$app = new Yaf_Application(APP_PATH."/conf/home.ini");
 // $app->bootstrap()->run();
-$app->getDispatcher()->dispatch(new Yaf_Request_Simple());
+// $app->getDispatcher()->dispatch(new Yaf_Request_Simple());
+
+// 不需要初始化的时候这样做
+// $app->getDispatcher()->dispatch($request);
+
+// 需要初始化的时候这样做
+$app->bootstrap()->getDispatcher()->dispatch($request);
