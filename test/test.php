6e23f73e0694edaeaf3f3de61ab0f6fa41cfc1de
lei.di <lei.di@huanxi.com>
2017-03-24 18:32:56 +0800

    update 324

--- test/test.php
+++ test/test.php
@@ -10,6 +10,26 @@ function test(){
     echo "123";
 } 
 
-test();
+// test();
+
+//l 声明为字符串 然后以数组方式赋值会报错
+function test_type() {
+    // 1
+    // http://php.net/manual/zh/language.types.string.php
+    //自 PHP 5.4 起字符串下标必须为整数或可转换为整数的字符串，否则会发出警告 
+    $str ="2222";
+    $str["a"] = "333";
+    echo $str;
+
+    // 2 它会先解析$str,然后再算偏移量
+    $str = "aaaaa";
+    $str["2"] = "333";
+    echo $str;  // aa3aa
+
+    //3 正确
+    $str = array();
+    $str["a"] = "333";
+}
+test_type();
 
 
